46c91b378956c601aa54118fd135f5794201e87c
frontden <korolenok@strateg.by>
2018-10-30 13:06:36 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Howl, Howler} from 'howler';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  disabled = false;
+
+  image = '../assets/bananStatic.jpg';
+
+  sound = new Howl({
+        src: ['../assets/test.mp3']
+    });
+
+    test() {
+        this.disabled = true;
+        this.image = '../assets/banan.gif';
+        this.sound.play();
+
+        setTimeout(() => {this.disabled = false; this.image = '../assets/bananStatic.jpg'; }, 5000);
+    }
+
+
+
 }
